16552b062a6dbd776a6c6c1e8a39c3b483c65ed0
Chee Zhiquan <zq@zv.sg>
2020-08-16 23:04:40 +0800

    Dispatch reset parts state after going back home page

--- src/views/Page3.tsx
+++ src/views/Page3.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import PageWrapper from '../components/PageWrapper';
 import Spacer from '../components/Spacer';
 import ProgressIndicator from '../components/ProgressIndicator';
 import Button from '../components/Button';
+import { clearParts } from '../redux/reducers/parts';
 
 const CURRENT_PAGE = 3;
 
@@ -24,16 +26,19 @@ const WideButton = styled(Button)`
   width: 15rem;
 `;
 
-const Page3 = () => (
-  <PageWrapper>
-    <ProgressIndicator totalPages={3} currentPage={CURRENT_PAGE} />
-    <Spacer size={4} />
-    <SuccessMessage>Success!</SuccessMessage>
-    <Spacer size={4} />
-    <CenterLink to="/">
-      <WideButton>Home</WideButton>
-    </CenterLink>
-  </PageWrapper>
-);
+const Page3 = () => {
+  const dispatch = useDispatch();
+  return (
+    <PageWrapper>
+      <ProgressIndicator totalPages={3} currentPage={CURRENT_PAGE} />
+      <Spacer size={4} />
+      <SuccessMessage>Success!</SuccessMessage>
+      <Spacer size={4} />
+      <CenterLink to="/">
+        <WideButton onClick={() => dispatch(clearParts())}>Home</WideButton>
+      </CenterLink>
+    </PageWrapper>
+  );
+};
 
 export default Page3;
